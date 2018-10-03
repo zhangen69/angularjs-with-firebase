f2f60ceacee747950c784b2aa7e9667bd8a6d6f5
Jacob <zhangen69@gmail.com>
2018-10-03 23:15:46 +0800

    update, firebase get worked as well

--- app/app.js
+++ app/app.js
@@ -1,20 +1,13 @@
 'use strict';
 
-var app = angular.module('app', ['ui.router', 'oc.lazyLoad', 'firebase']);
+var app = angular.module('app', [
+    'ui.router', 
+    'oc.lazyLoad', 
+    // 'firebase'
+]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    // firebase initialize
-    firebase.initializeApp({
-        "apiKey": "AIzaSyA2JIDbyPTyWoHctTFoOgzf-e4vmreyKtM",
-        "databaseURL": "https://fir-weaver-pwa-app.firebaseio.com",
-        "storageBucket": "fir-weaver-pwa-app.appspot.com",
-        "authDomain": "fir-weaver-pwa-app.firebaseapp.com",
-        "messagingSenderId": "381435042864",
-        "projectId": "fir-weaver-pwa-app"
-    });
-
-    firebase.app();
-      
+    //firebase initilize
     firebase.firestore().settings({ timestampsInSnapshots: true });
 
     $urlRouterProvider.otherwise('/home');
